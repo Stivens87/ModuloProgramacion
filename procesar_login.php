c1dfbd984f15818d9a6b19edf72ee9e20bd42fa3
JuanPatinoV <juanpatino273360@correo.itm.edu.co>
2023-04-13 23:28:38 -0500

    credenciales_incorrec

--- procesar_login.php
+++ procesar_login.php
@@ -2,27 +2,21 @@
 require_once 'usuario.php';
 session_start();
 
+
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-<<<<<<< HEAD
 $usuario = new Usuario();
 $nombre =  $usuario->buscarUsuario($username, $password);
 if ($nombre == null) {
 
     echo "<script>alert('Error de usuario o Contraseña'); window.location='Index.php';</script>";
 
-=======
-if (!empty($username) && !empty($password)) {
-    $usuario = new Usuario();
-    $nombre = $usuario->buscarUsuario($username, $password);
-    if ($nombre == null) {
-        echo "<script>alert('Error de usuario o Contraseña'); window.location='Index.php';</script>";
-    } else {
-        // echo "<script>alert('Usuario Logueado. Bienvenido:  $nombre');</script>";
-        $_SESSION["username"] = $username;
-    }
->>>>>>> 38563f5e1e469fc2d67424102de543e603a5fa2b
 } else {
-    echo "<script>alert('Por favor, ingrese su nombre de usuario y contraseña');</script>";
+    // echo "<script>alert('Usuario Logueado. Buenvenido:  $nombre');</script>";
+    $_SESSION["username"] = $username;
+    header("Location: inicio.php");
+    exit();
 }
+
+?>
