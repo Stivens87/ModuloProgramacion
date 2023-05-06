496950983fb288ed077835960501f432befaef21
Juan Robayo <juanrobayo288277@correo.itm.edu.co>
2023-05-05 21:17:54 -0500

    Tiempo inactivo

--- Views/producto/verificar_sesion.php
+++ Views/producto/verificar_sesion.php
@@ -1,30 +1,32 @@
 <?php
 // Configuración
-$tiempo_inactivo = 300; // 5 minutos
+$tiempo_inactivo = 5; // 5 minutos
 
 // Iniciar sesión
 session_start();
 
 if(!isset($_SESSION["username"])) {
-header("Location: index.php");
- exit();
+    header("Location: index.php");
+    exit();
 }
+
+// Obtener el nombre de usuario
 $username = $_SESSION["username"];
+
 // Verificar la actividad del usuario
 if (isset($_SESSION['ultimo_actividad']) && (time() - $_SESSION['ultimo_actividad']) > $tiempo_inactivo) {
-	
-	session_unset();
-	session_destroy();
-	echo "<script>alert('Su sesión ha sido cerrada por inactividad. Por favor inicie sesión de nuevo.');</script>";
-
- 	header("Location: Index.php");
-	exit();
+    // Si ha pasado demasiado tiempo, cerrar sesión por inactividad
+    session_unset();
+    session_destroy();
+    echo "<script>alert('Su sesión ha sido cerrada por inactividad. Por favor inicie sesión de nuevo.');</script>";
+    echo "<script>window.location.href = 'index.php';</script>"; // Redirigir forzosamente al usuario a la página de inicio de sesión
+    exit();
 } else {
-// Actualizar el tiempo de última actividad
-$_SESSION['ultimo_actividad'] = time();
+    // Actualizar el tiempo de última actividad
+    $_SESSION['ultimo_actividad'] = time();
 }
-
 ?>
+
 
 
 
