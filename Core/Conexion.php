f0706b6bc5cc010ab5c0ddc9169001a1207ec1eb
JuanPatinoV <juanpatino273360@correo.itm.edu.co>
2023-06-10 08:42:33 -0500

    terminado-10-06-2023

--- Core/Conexion.php
+++ Core/Conexion.php
@@ -33,22 +33,4 @@ class Conexion
         }
     }
 
-    static function dbconexion()
-    {
-        $host1 = "DESKTOP-H2JVRGK\SQLEXPRESS01";
-        $username1 = "usuarioArgos";
-        $password1 = "qwerty";
-        $dbname1 = "DBprogramacion";
-        $port1 = 1433;
-
-        try {
-            $conexion = new PDO("sqlsrv:server = $host1,$port1; Database = $dbname1", $username1, $password1);
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conexion;
-            
-        } catch (PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
-        }
-        
-    }
 }
